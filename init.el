776e8a92adef660ba407d728934ae6d026aec5a2
AlgorithmHaGadol <1337softwaredevelopment@gmail.com>
2025-03-17 17:15:43 +0200

    Add UI Changes
    - added which-key
    - added cape for better autocomplete
    - added dashboard
    - added rainbow-delimiters rainbow-mode rainbow-identifiers
    - added yasnippet with extras
    - font changes

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-(defvar ovadia/default-font-size 120)
-(defvar ovadia/default-variable-font-size 120)
+(defvar ovadia/default-font-size 110)
+(defvar ovadia/default-variable-font-size 110)
 (defvar ovadia/frame-transparency '(90 . 90))
 
 (set-frame-parameter (selected-frame) 'alpha ovadia/frame-transparency)
@@ -19,6 +19,7 @@
 (electric-pair-mode t)
 (column-number-mode t)
 (save-place-mode t)
+(savehist-mode t)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -52,7 +53,8 @@
 (use-package doom-modeline
   :init (doom-modeline-mode)
   :config
-  (setq doom-modeline-height 30))
+  (setq doom-modeline-height 30
+	doom-modeline-minor-modes t))
 
 (use-package doom-themes
   :init (load-theme 'doom-dracula t))
@@ -88,6 +90,100 @@
 
 (use-package nerd-icons-corfu
   :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package cape
+  :init
+  ;;(add-hook 'completion-at-point-functions #'cape-dabbrev)
+  ;;(add-hook 'completion-at-point-functions #'cape-file)
+  ;;(add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;;(add-hook 'completion-at-point-functions #'cape-history)
+  )
+
+(defun cape-all()
+  (cape-wrap-super #'yasnippet-capf #'cape-dabbrev #'cape-file #'cape-elisp-block #'cape-history))
+(use-package yasnippet-snippets)
+(use-package yasnippet
+  :ensure t
+  :init (yas-global-mode))
+(use-package yasnippet-capf
+  :after cape
+  :init (add-hook 'completion-at-point-functions #'cape-all))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package rainbow-identifiers
+  :hook (prog-mode . rainbow-identifiers-mode))
+
+(use-package rainbow-mode 
+  :hook (prog-mode . rainbow-mode))
+
+(add-to-list 'load-path
+             (expand-file-name "~/.config/emacs30/edwina"))
+
+(add-to-list 'nerd-icons-mode-icon-alist '(edwina-mode nerd-icons-codicon "nf-cod-layout" :face nerd-icons-dblue))
+(require 'edwina)
+(edwina-setup-dwm-keys)
+(edwina-mode 1)
+
+(use-package minions
+  :ensure t
+  :config (minions-mode 1))
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config (which-key-mode))
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :bind
+  ([remap describe-function] . helpful-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (setq dashboard-center-content t
+	dashboard-startup-banner "~/.config/emacs30/ovadiaTech_Logo.png"
+	dashboard-banner-logo-title "Work SMART, not HARD!"
+	dashboard-display-icons-p t
+	dashboard-icon-type 'nerd-icons
+	dashboard-set-file-icons t
+	dashboard-set-heading-icons t)
+  (dashboard-modify-heading-icons '((recents . "nf-oct-file_code")
+				    (bookmarks . "nf-oct-bookmark")
+				    (agenda . "nf-oct-tasklist")
+				    (projects . "nf-oct-code")))
+  (dashboard-setup-startup-hook))
+
+(use-package magit
+  :commands magit-status
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+
+(set-face-attribute 'default nil :font "JetBrainsMono NF" :height ovadia/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono NF" :height ovadia/default-font-size)
+
+(defvar ligatures-JetBrainsMono
+    '("--" "---" "==" "===" "!=" "!==" "=!=" "=:=" "=/=" "<=" ">=" "&&" "&&&" "&=" "++" "+++"
+      "***" ";;" "!!" "??" "?:" "?." "?=" "<:" ":<" ":>" ">:" "<>" "<<<" ">>>" "<<" ">>" "||" "-|"
+      "_|_" "|-" "||-" "|=" "||=" "##" "###" "####" "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:"
+      "#!" "#=" "^=" "<$>" "<$" "$>" "<+>" "<+ +>" "<*>" "<* *>" "</" "</>" "/>" "<!--"
+      "<#--" "-->" "->" "->>" "<<-" "<-" "<=<" "=<<" "<<=" "<==" "<=>" "<==>" "==>" "=>"
+      "=>>" ">=>" ">>=" ">>-" ">-" ">--" "-<" "-<<" ">->" "<-<" "<-|" "<=|" "|=>" "|->" "<-"
+      "<~~" "<~" "<~>" "~~" "~~>" "~>" "~-" "-~" "~@" "[||]" "|]" "[|" "|}" "{|" "[<" ">]"
+      "|>" "<|" "||>" "<||" "|||>" "|||>" "<|>" "..." ".." ".=" ".-" "..<" ".?" "::" ":::"
+      ":=" "::=" ":?" ":?>" "//" "///" "/*" "*/" "/=" "//=" "/==" "@_" "__"))
+  (use-package ligature
+    :config
+    (ligature-set-ligatures 'prog-mode ligatures-JetBrainsMono)
+    (ligature-set-ligatures 'org-mode ligatures-JetBrainsMono)
+    (global-ligature-mode 1))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
